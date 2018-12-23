4290985b1590587de7715aa0be13d0e7190be81f
Chen <chenyu.zhu86@gmail.com>
2018-12-22 20:53:19 -0500

    broke routes up into separate files

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use('/product', (req, res) => {
-  console.log(req.body)
-  res.redirect('/')
-})
+app.use(bodyParser.urlencoded({extended: false}))
 
+app.use(adminRoutes)
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello From Express!</h1>')
-})
-
-
+app.use(shopRoutes)
 
 app.listen(3000)
