003cfdf21d3063e3961cb8e3b2beb2a9684fb3c7
Chen <chenyu.zhu86@gmail.com>
2018-12-23 10:46:24 -0500

    created views using ejs

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -14,8 +17,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.use((req, res, next) => {
+  res.status(404).render("404", {
+    pageTitle: "Page Not Found",
+    path: "/"
+  });
 });
 
 app.listen(3000);
