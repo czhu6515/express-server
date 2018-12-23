61ae01dbf621674cb2d6d80d1648d761c2ab8272
Chen <chenyu.zhu86@gmail.com>
2018-12-22 22:11:10 -0500

    created  route

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/product', (req, res) => {
+router.get('/add-product', (req, res) => {
   res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-router.post('/product', (req, res) => {
+router.post('/add-product', (req, res) => {
   console.log(req.body)
   res.redirect('/')
 })
